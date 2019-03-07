3a25c285d971a337a0338a3dba30d6a902bbc462
kczereczon <kczereczon@gmail.com>
2019-03-07 08:53:06 +0100

    Adding missing project files

---
+++ desktop/src/com/kczereczon/roguelike/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kczereczon.roguelike.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kczereczon.roguelike.RoguelikeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RoguelikeGame(), config);
+	}
+}
